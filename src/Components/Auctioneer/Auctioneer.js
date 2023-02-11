866e3681b8c6ef49d2b905651f9002574f30060a
KaranSonik <sonikkaran@gmail.com>
2023-02-11 19:06:04 +0000

    Karan - Auctioneer

--- src/Components/Auctioneer/Auctioneer.js
+++ src/Components/Auctioneer/Auctioneer.js
@@ -1,5 +1,36 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom';
+import Navbar from '../Navbar';
+import dummyData from '../../utils/dummydata'
+import './auctioneer.css'
+import { useSelector } from 'react-redux';
+
+
+const Items = ({ itemId, auctioneer, image, initialPrice, currentBid}) => {
+  const cc_account = useSelector(state=>state.connected_account)
+
+  if (cc_account == auctioneer) {
+    return (
+      <li className='item'>
+        <img src={image} height='100px' width="100px"></img>
+        <div className='itemDetails'>
+          <span>{itemId}</span>
+          <p className='auctioneer'>{auctioneer}</p>
+          <div>
+            <p className='initialP'>{initialPrice}</p>
+            <p className='currP'>{currentBid}</p>
+          </div>
+  
+        </div>
+      </li>
+    )
+  }
+  else {
+    return null
+  }
+ 
+}
+
 const Auctioneer = () => {
     const navigate = useNavigate();
     const handleMainClick = ()=>
@@ -7,10 +38,23 @@ const Auctioneer = () => {
         navigate("/");
     }
 
+
+
   return (
     <div>
+      <Navbar/>
         <button onClick={handleMainClick}>Main</button>
       <h1>Auctioneer</h1>
+
+      <div className='itemsWrapper'>
+        <ul className='itemsList'>
+
+          {dummyData.reverse().map((items, i) => (
+            <Items key={i} {...items}/>
+            ))}
+        </ul>
+      </div>
+      
     </div>
   )
 }
---
+++ src/utils/dummydata.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        "itemId": 1,
+        "auctioneer": "0x7c0b4A5195b5bC61Ea177a99ebe7567355FD5737",
+        "image": "https://dogecoin.com/assets/images/doge.svg",
+        "initialPrice": 10,
+        "currentBid": 0,
+        "soldTo": "",
+    },
+    {
+        "itemId": 2,
+        "auctioneer": "0x7c0b4A5195b5bC61Ea177a99ebe7567355FD5737",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/800px-Bitcoin.svg.png",
+        "initialPrice": 40,
+        "currentBid": 0,
+        "soldTo": "",
+    },
+    {
+        "itemId": 3,
+        "auctioneer": "0x7c0b4A5195b5bC61Ea177a99ebe7567355FD5737",
+        "image": "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp",
+        "initialPrice": 60,
+        "currentBid": 0,
+        "soldTo": "",
+    }
+]
