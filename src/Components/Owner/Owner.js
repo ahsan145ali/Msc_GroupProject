9444a74ce90a64c9041624e3c0699451c4d3c4ef
Hania <hania.riim19@gmail.com>
2023-03-01 12:47:43 +0000

    changes

--- src/Components/Owner/Owner.js
+++ src/Components/Owner/Owner.js
@@ -1,17 +1,95 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import IPFS from '../../IPFS';
+import RegisterAuctioneer from '../Auctioneer/RegisterAuctioneer';
+
 const Owner = () => {
-    const navigate = useNavigate();
-    const handleMainClick = ()=>
+
+  const navigate = useNavigate();
+  const connected_acc = useSelector(state=>state.connected_account);
+  const auction_contract = useSelector(state=>state.auction_contract);  
+  const [numAddresses, setNumAddresses] = useState(null)
+  const [registeredAuctioneers, setRegisteredAuctioneers] = useState([])
+
+  
+
+
+  useEffect(()=>{
+    getRegisteredAuctioneers();
+    
+  },[])
+
+  const handleMainClick = ()=>
+  {
+      navigate("/");
+  }
+
+  const handleAddressClick = ()=>
+  {
+
+  }
+
+  const getRegisteredAuctioneers = async () => {
+
+    await auction_contract.methods.GetAddressesCount().call({
+      from: connected_acc[0]
+    }).then((receipt) => {
+
+      console.log("R: ", receipt)
+      setNumAddresses(receipt)
+    })
+
+    
+  }
+
+  const getRegisteredAddresses = async (event) => {
+    let arr = []
+    //setRegisteredAuctioneers([])
+    for (let i = 0; i < numAddresses; i++)
     {
-        navigate("/");
+      await auction_contract.methods.GetRegisteredAddresses(i).call({
+        from:connected_acc[0]
+      }).then((res) => {
+        
+        registeredAuctioneers.push({addr:res });
+        arr.push(res);
+        //setRegisteredAuctioneers({ ...registeredAuctioneers }, res);
+        console.log("res: " + res)
+       // console.log("Arr: " + registeredAuctioneers);
+ 
+      })  
+    }  
+    console.log("arr: " + registeredAuctioneers)
+   // setRegisteredAuctioneers(arr);
+    
+    //event.preventDefault();
+    
     }
-  return (
-    <div>
-      <button onClick={handleMainClick}>Main</button>
-      <h1>Owner</h1>
-    </div>
-  )
+
+    return (
+      <>
+        <div>
+            <button onClick={handleMainClick}>Main</button>
+            <h1>Owner</h1>
+        </div>
+
+        <div>
+          <button onClick={getRegisteredAddresses}>Get ADDR</button>
+        </div>
+        {console.log("len: " + registeredAuctioneers.length)}
+        {registeredAuctioneers.map(item => {
+          <div>
+            
+    
+              {item.addr}
+        
+            
+          </div>
+        })}
+        
+      </>
+    )
 }
 
 export default Owner
